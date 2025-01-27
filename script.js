9b2065faa3725f79a67fdf0ec1d627f55c68b423
Wittner Andrews <wittnerandrews@gmail.com>
2025-01-27 00:12:34 -0600

    playGame added

--- script.js
+++ script.js
@@ -15,35 +15,42 @@ function getHumanChoice(){
 const humanChoice = getHumanChoice();
 console.log(humanChoice);
 
-let humanScore = 0;
-let computerScore = 0;
 
-function playRound(computerChoice, humanChoice){
-    if (computerChoice === humanChoice) {
-        console.log("You both chose the same thing. It's a tie!");
-    }
-    else if (humanChoice === "r" && computerChoice === "p") {
-        console.log("You chose rock and the computer chose paper. You lose!")
-        computerScore++;
-    }
-    else if (humanChoice === "p" && computerChoice === "r" ) {
-        console.log("You chose paper and the computer chose rock. You win!")
-        humanScore++;
-    }
-    else if (humanChoice === "p" && computerChoice === "s") {
-        console.log("You chose paper and the computer chose scissors. You lose!")
-        computerScore++;
-    }
-    else if (humanChoice === "r" && computerChoice === "s") {
-        console.log("You chose rock and the computer chose scissors. You win!")
-        humanScore++;
-    }
-    else if (humanChoice === "s" && computerChoice === "p") {
-        console.log("You chose scissors and the computer chose paper. You win!")
-        humanScore++;
-    }    
-    else if (humanChoice === "s" && computerChoice === "r") {
-        console.log("You chose scissors and the computer chose rock. You lose!")
-        computerScore++;
+while (humanScore && computerScore < 5){
+    playRound();
+}
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(computerChoice, humanChoice){
+        if (computerChoice === humanChoice) {
+            console.log("You both chose the same thing. It's a tie!");
+        }
+        else if (humanChoice === "r" && computerChoice === "p") {
+            console.log("You chose rock and the computer chose paper. You lose!")
+            computerScore++;
+        }
+        else if (humanChoice === "p" && computerChoice === "r" ) {
+            console.log("You chose paper and the computer chose rock. You win!")
+            humanScore++;
+        }
+        else if (humanChoice === "p" && computerChoice === "s") {
+            console.log("You chose paper and the computer chose scissors. You lose!")
+            computerScore++;
+        }
+        else if (humanChoice === "r" && computerChoice === "s") {
+            console.log("You chose rock and the computer chose scissors. You win!")
+            humanScore++;
+        }
+        else if (humanChoice === "s" && computerChoice === "p") {
+            console.log("You chose scissors and the computer chose paper. You win!")
+            humanScore++;
+        }    
+        else if (humanChoice === "s" && computerChoice === "r") {
+            console.log("You chose scissors and the computer chose rock. You lose!")
+            computerScore++;
+        }
     }
 }
