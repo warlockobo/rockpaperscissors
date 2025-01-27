5ff23a4c94cf3482fc0f3039352472740a859080
Wittner Andrews <wittnerandrews@gmail.com>
2025-01-26 23:06:30 -0600

    functional random function

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Hello World!");
+
+function getComputerChoice(){
+   const computerchoice = Math.floor(Math.random() * 3);
+
+   return computerchoice;
+}
+
+computerchoice = getComputerChoice();
+console.log(computerchoice);
