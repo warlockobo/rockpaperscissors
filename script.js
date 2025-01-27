1efc5c5c28293b6d3b0af4e5392a131ddfc526a0
Wittner Andrews <wittnerandrews@gmail.com>
2025-01-26 23:36:32 -0600

    humanchoice and score

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ function getComputerChoice(){
 }
 const computerchoice = getComputerChoice();
 console.log(computerchoice)
+
+function getHumanChoice(){
+    const humanChoice = prompt("Choose rock, paper, or scissors.");
+
+    return humanChoice.toLowerCase();
+}
+
+const humanChoice = getHumanChoice();
+console.log(humanChoice);
+
+let humanScore = 0;
+let computerScore = 0;
