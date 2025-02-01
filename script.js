6c92b5d7d2734d121b615426801bce1617439b06
Wittner Andrews <wittnerandrews@gmail.com>
2025-01-31 21:39:54 -0600

    scores added

--- script.js
+++ script.js
@@ -14,38 +14,50 @@ function playGame() {
             return choices[index];
         }
         const computerChoice = getComputerChoice();
-        let results = document.getElementById("results")
+        const container = document.getElementById("container");
+        const results = document.getElementById("results")
+        const score = document.getElementById("score");
+
+
 
         if (computerChoice === humanChoice) {
             results.textContent = "You both chose the same thing. It's a tie!";
+            score.textContent = "User score: " + humanScore + " Computer Score: " + computerScore;
         }
         else if (humanChoice === "r" && computerChoice === "p") {
             results.textContent = "You chose rock and the computer chose paper. You lose!";
             computerScore++;
+            score.textContent = "User score: " + humanScore + " Computer Score: " + computerScore;
         }
         else if (humanChoice === "p" && computerChoice === "r" ) {
             results.textContent = "You chose paper and the computer chose rock. You win!"
             humanScore++;
+            score.textContent = "User score: " + humanScore + " Computer Score: " + computerScore;
         }
         else if (humanChoice === "p" && computerChoice === "s") {
             results.textContent = "You chose paper and the computer chose scissors. You lose!"
             computerScore++;
+            score.textContent = "User score: " + humanScore + " Computer Score: " + computerScore;
         }
         else if (humanChoice === "r" && computerChoice === "s") {
             results.textContent = "You chose rock and the computer chose scissors. You win!"
+            score.textContent = "User score: " + humanScore + " Computer Score: " + computerScore;
             humanScore++;
+            score.textContent = "User score: " + humanScore + " Computer Score: " + computerScore;
+
         }
         else if (humanChoice === "s" && computerChoice === "p") {
             results.textContent = "You chose scissors and the computer chose paper. You win!"
             humanScore++;
+            score.textContent = "User score: " + humanScore + " Computer Score: " + computerScore;
+
         }    
         else if (humanChoice === "s" && computerChoice === "r") {
             results.textContent = "You chose scissors and the computer chose rock. You lose!"
             computerScore++;
+            score.textContent = "User score: " + humanScore + " Computer Score: " + computerScore;
+
         }
-    }
-    while (humanScore < 5 && computerScore < 5){
-        playRound();
     }
 }
 playGame();
