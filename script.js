00f281e94beb3585206b364d468a85fd4a4339af
Wittner Andrews <wittnerandrews@gmail.com>
2025-01-31 21:12:19 -0600

    all console.log turned to dom

--- script.js
+++ script.js
@@ -14,32 +14,33 @@ function playGame() {
             return choices[index];
         }
         const computerChoice = getComputerChoice();
+        let results = document.getElementById("results")
 
         if (computerChoice === humanChoice) {
-            console.log("You both chose the same thing. It's a tie!");
+            results.textContent = "You both chose the same thing. It's a tie!";
         }
         else if (humanChoice === "r" && computerChoice === "p") {
-            console.log("You chose rock and the computer chose paper. You lose!")
+            results.textContent = "You chose rock and the computer chose paper. You lose!";
             computerScore++;
         }
         else if (humanChoice === "p" && computerChoice === "r" ) {
-            console.log("You chose paper and the computer chose rock. You win!")
+            results.textContent = "You chose paper and the computer chose rock. You win!"
             humanScore++;
         }
         else if (humanChoice === "p" && computerChoice === "s") {
-            console.log("You chose paper and the computer chose scissors. You lose!")
+            results.textContent = "You chose paper and the computer chose scissors. You lose!"
             computerScore++;
         }
         else if (humanChoice === "r" && computerChoice === "s") {
-            console.log("You chose rock and the computer chose scissors. You win!")
+            results.textContent = "You chose rock and the computer chose scissors. You win!"
             humanScore++;
         }
         else if (humanChoice === "s" && computerChoice === "p") {
-            console.log("You chose scissors and the computer chose paper. You win!")
+            results.textContent = "You chose scissors and the computer chose paper. You win!"
             humanScore++;
         }    
         else if (humanChoice === "s" && computerChoice === "r") {
-            console.log("You chose scissors and the computer chose rock. You lose!")
+            results.textContent = "You chose scissors and the computer chose rock. You lose!"
             computerScore++;
         }
     }
